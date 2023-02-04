2cc4046b6b19e47a7a9ec39b9f2b6eaaca57f0c5
KKK <73897987+kkk-bot@users.noreply.github.com>
2023-02-04 20:58:04 +0300

    Making the choices
    You can play against computer or player

--- XO game.cpp
+++ XO game.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
+#include <string>
 using namespace std;
-
 bool isTheGameStill(int board[3][3])
 {
     for (int i = 0; i < 3; i++)
@@ -41,7 +41,7 @@ void printBoard(int board[3][3])
             if (board[i][j] > 9)
                 cout << (char)board[i][j];
             else
-                cout << board[i][j];
+                cout << ' ';
             if (j < 2)
                 cout << "  |  ";
         }
@@ -58,6 +58,7 @@ void changePlayer(char& x)
     else
         x = 'X';
 }
+
 bool checkIfValueExist(int x, int board[3][3])
 {
     for (int i = 0; i < 3; i++)
@@ -68,21 +69,83 @@ bool checkIfValueExist(int x, int board[3][3])
     }
     return false;
 }
-
-int main(void)
+void tutorial()
 {
-    char player = 'X';
+    int help = 1;
+    cout << "\n\nHELP:\n";
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << help;
+            if (j < 2)
+                cout << "  |  ";
+            help++;
+        }
+
+        cout << endl;
+        if (i < 2)
+            cout << "--------------" << endl;
+    }
+    cout << "\n\n";
+}
+void checkTurn(char player)
+{
+    if (player == 'X')
+        cout << "Player (X): ";
+    else
+        cout << "Player (O): ";
+}
+
+
+
+int main()
+{
+    srand(time(0));
+    bool isComputer = false;
     int n;
+    char list[2] = {'X', 'O'};
+    char jika = 1 + (rand() % 2);
+    char player = list[jika-1];
+    int choice;
     int board[3][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
     // X = 88
     // O = 79
+    
+
+    cout << " ------- XO game -------\n";
+    cout << "1. Play against computer\n";
+    cout << "2. Play against player\n";
+    cout << "3. Exit\n";
+    cin >> choice;
+    while (choice < 1 || choice>3)
+    {
+        cout << "Invalid choice: ";
+        cin >> choice;
+    }
+    if (choice == 3)
+        return 0;
     printBoard(board);
-
-    cout << "Player (X): ";
-
+    tutorial();
+    checkTurn(player);
     while (isTheGameStill(board))
     {
-        cin >> n;
+        if (choice == 1)
+            if (isComputer)
+            {
+                n = 1+(rand() % 9);
+                while (!checkIfValueExist(n, board))
+                {
+                    n = rand() % 9 + 1;
+                }
+            }
+            else
+            {
+                cin >> n;
+            }
+        else if (choice == 2)
+            cin >> n;
+
         if (n < 1 || n>9)
         {
             cout << "invalid value: ";
@@ -112,34 +175,32 @@ int main(void)
         }
         system("cls");
         printBoard(board);
+        tutorial();
         if (checkBoard(board))
-            if (player == 'O')
-            {
-                cout << "The winner is Player (O)\n";
-                return 0;
-            }
-            else
-            {
-                cout << "The winner is Player (X)\n";
-                return 0;
-            }
+        {
+            if (isComputer && choice == 1)
+                cout << "The winner is Computer\n";
+
+            if (player == 'X')
+                cout << "The winner is Player (X)";
+            else if (player == 'O')
+                cout << "The winner is Player (O)";
+            return 0;
+        }
+
+            
 
         if (isTheGameStill(board) == false)
         {
-             cout << "The game is is Draw." << endl;
+             cout << "The game is Draw." << endl;
              return 0;
         }
-
-        if (player == 'O')
-        {
-            cout << "Player (X): ";
-            changePlayer(player);
-        }
+        changePlayer(player);
+        checkTurn(player);
+        if (isComputer)
+            isComputer = false;
         else
-        {
-            cout << "Player (O): ";
-            changePlayer(player);
-        }
+            isComputer = true;
     }
     return 0;
 }
